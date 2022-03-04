7c81c43aef02cc0c338ae89ee205f2ec39c3a792
Daria <you@example.com>
2022-03-04 22:46:22 +0300

    .

--- 8/9.py
+++ 8/9.py
@@ -2,17 +2,23 @@ import os
 import glob
 from os import path
 
-path_1 = r'C:\Users\Daria\Desktop\неотология'
-    
-if path.exists('path_1'):
-    pattern = '*.txt'
-    glob_path = os.path.join('path_1', pattern)
-    list_files = glob.glob(glob_path)
-    new_file = 'new_file.all'
-    if list_files:
-        for file_name in list_files:
-             with open(file_name, 'r', encoding='UTF-8') as fr, open(new_file, 'a', encoding='UTF-8') as fw:
-                 fw.write(f'\n\n------------ {file_name}\n\n')
-                 for line in fr:
-                    fw.write(line) 
+def comparator(doc):
+    return len(doc)
 
+def write_with_sort(direct):
+    if path.exists(direct):
+        files = []
+        pattern = '*.txt'
+        glob_path = os.path.join(direct, pattern)
+        list_files = glob.glob(glob_path)
+        for file in list_files:
+            with open(file) as f:
+                files.append(f.readlines())
+        files.sort(key=comparator)
+        print(files)
+        with open('all.txt', 'w') as f:
+            for file in files:
+                f.writelines(file)
+                f.write('\n')
+
+write_with_sort(input())
