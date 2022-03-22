717b9b1a1f289afbd34fea49654770179edaa644
Daria <you@example.com>
2022-03-22 16:29:11 +0300

    _____

--- 8/9-3.py
+++ 8/9-3.py
@@ -10,11 +10,9 @@ for i in my_list:
         my_dict['count_lines'] = len(lines)
         my_dict['lines'] = lines
         dict.append(my_dict)
-res=sorted(dict, key=lambda x: x['count_lines'])
-
 
 with open('new.txt', "wb") as fh:
-    pickle.dump(dict, fh) 
+    pickle.dump(sorted(dict, key=lambda x: x['count_lines']), fh) 
 
 dict_2 = []
 with open('new.txt', "rb") as fh:
@@ -25,6 +23,6 @@ print(dict_2)
 # Вариант, представленный ниже не работает у меня, все свои пределы умственных способностей исчерпала
 #with open('all.txt', 'w') as f:
  #    f.write(dict["name_file"] + "\n")
-  #   f.write(str(dict["count_lines"]) + "\n")
+  #   f.write(dict["count_lines"] + "\n")
   #   f.write(dict["lines"] + "\n")
    #  f.close()  
