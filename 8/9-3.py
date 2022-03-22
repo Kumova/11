1e6ff154f5747b841a03db53fb47e730fbf111d0
Daria <you@example.com>
2022-03-22 15:49:19 +0300

    ---

---
+++ 8/9-3.py
@@ -0,0 +1,30 @@
+import pickle
+
+my_list = ['1.txt', '2.txt', '3.txt']
+dict = []
+for i in my_list:
+    my_dict = {}
+    with open(i, 'r', encoding='utf-8') as file:
+        my_dict['name_file'] = i
+        lines = file.readlines()
+        my_dict['count_lines'] = len(lines)
+        my_dict['lines'] = lines
+        dict.append(my_dict)
+res=sorted(dict, key=lambda x: x['count_lines'])
+
+
+with open('new.txt', "wb") as fh:
+    pickle.dump(dict, fh) 
+
+dict_2 = []
+with open('new.txt', "rb") as fh:
+    dict_2 = pickle.load(fh)  
+print(dict_2)  
+
+
+# Вариант, представленный ниже не работает у меня, все свои пределы умственных способностей исчерпала
+#with open('all.txt', 'w') as f:
+ #    f.write(dict["name_file"] + "\n")
+  #   f.write(str(dict["count_lines"]) + "\n")
+  #   f.write(dict["lines"] + "\n")
+   #  f.close()  
